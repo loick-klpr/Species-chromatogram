7cf61dc026d90f17d4d6b7519dd2119854969797
loick-klpr <79358442+loick-klpr@users.noreply.github.com>
2022-02-21 13:51:31 +0100

    Updated Matlab functions.

---
+++ niche_difer_spbis.m
@@ -0,0 +1,21 @@
+function y=niche_difer_spbis(sp_chr,T)
+% To use in 'combina_niche3.m'
+% INPUTS:
+%
+% sp_chr: three dimensional matrices with the species chromatograms (alpha category by p environmental variables by species)
+% T: the threshold of minimal abundance in a category for the niche breadth estimation
+% 
+% OUTPUTS:
+%
+% y: index D
+%
+% Grégory Beaugrand Avril 2021
+
+[~,~,z]=size(sp_chr);
+y=zeros(z,z)+nan;
+for i=1:z
+    for j=i+1:z
+        y(i,j)=niche_difer2bis(squeeze(sp_chr(:,:,i)),squeeze(sp_chr(:,:,j)),T);
+    end
+end
+
